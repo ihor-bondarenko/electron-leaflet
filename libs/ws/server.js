318adfb3eec9374c4729cabfe8ac18286a818b6b
ihor <guest@localhost.localdomain>
2017-05-22 15:17:14 -0400

    fx

--- libs/ws/server.js
+++ libs/ws/server.js
@@ -1,4 +1,4 @@
-const WebSocket = require('ws');
+/*const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8081 });
 const db = require('../rthinkdb/server');
 
@@ -23,4 +23,33 @@ const interval = setInterval(function ping() {
         ws.isAlive = false;
         ws.ping('', false, true);
     });
-}, 30000);
+}, 30000);*/
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8089);
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+io.on('connection', function (socket) {
+    socket.on('news', function (data) {
+        console.log(data);
+    });
+});
+
+setInterval(function(){
+    console.log('emit');
+    io.sockets.emit('news', { hello: 'world' });
+}, 1000);
